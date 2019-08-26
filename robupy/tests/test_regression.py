54251eba8438481a4e646f2be8a2a7caee41709d
MaxBlesch <s6mables@uni-bonn.de>
2019-08-26 10:44:50 +0200

    Deleted test for unnecessary function.

--- robupy/tests/test_regression.py
+++ robupy/tests/test_regression.py
@@ -1,18 +1,4 @@
 """This module contains the regression tests."""
-# import pickle as pkl
-#
-# import numpy as np
-#
-# from robupy.config import PACKAGE_DIR
-
-
-# def test_1():
-#     """This tests runs a couple of regression tests."""
-#     tests = pkl.load(open(PACKAGE_DIR +
-#     '/tests/regression_vault.robupy.pkl', 'rb'))[:5]
-#     for test in tests:
-#         rslt, args = test
-#         np.testing.assert_almost_equal(get_multiplier_evaluation(*args), rslt)
 
 from numpy.testing import assert_array_almost_equal
 from robupy.auxiliary import (
@@ -26,7 +12,7 @@ from robupy.tests.pre_numba.auxiliary import (
 from robupy.tests.auxiliary import get_request
 
 
-def test_2():
+def test_1():
     x, v, q, beta, gamma, is_cost = get_request()
     assert_array_almost_equal(
         pre_numba_get_worst_case_outcome(v, q, beta, is_cost),
@@ -34,7 +20,7 @@ def test_2():
     )
 
 
-def test_3():
+def test_2():
     x, v, q, beta, gamma, is_cost = get_request()
     assert_array_almost_equal(
         get_worst_case_probs(v, q, beta), pre_numba_get_worst_case_probs(v, q, beta)
