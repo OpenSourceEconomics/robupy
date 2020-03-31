1d75274b7c9139e314b8135e1184eeac2c40efc6
MaxBlesch <s6mables@uni-bonn.de>
2020-03-31 16:39:58 +0200

    Drafting on a package.

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import Command
+from setuptools import find_packages
+from setuptools import setup
+
+# Package meta-data.
+NAME = "robupy"
+DESCRIPTION = (
+    ""
+)
+URL = ""
+EMAIL = ""
+AUTHOR = ""
+
+
+setup(
+    name=NAME,
+    version="0.1.0",
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=("tests",)),
+    license="MIT",
+    include_package_data=True,
+)
