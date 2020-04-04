5899edd5b69257e719f8920cc9b47c23fc1619b6
MaxBlesch <s6mables@uni-bonn.de>
2020-04-04 12:28:17 +0200

    Adjusted comment. Added Codecov badge.

--- robupy/__init__.py
+++
@@ -1,6 +0,0 @@
-import socket
-import warnings
-
-# This turns of the RuntimeWarnings during production so that they do not clutter the log files.
-if "heracles" not in socket.gethostname():
-    warnings.filterwarnings("ignore", category=RuntimeWarning, module="scipy")
